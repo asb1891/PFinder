e9a5498a000211d75a1fb6d665073f61e44e57ef
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-11 12:05:22 -0400

    firebase config

---
+++ database/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2NtILPT5dIaXmOra-zvZ8xOCwKWkppVE",
+  authDomain: "petswipe-95c0e.firebaseapp.com",
+  projectId: "petswipe-95c0e",
+  storageBucket: "petswipe-95c0e.appspot.com",
+  messagingSenderId: "819400705359",
+  appId: "1:819400705359:web:153f2ff578fedb8a5df13a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
