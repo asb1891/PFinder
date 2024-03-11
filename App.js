255db6f46813ff4efb12f615cd68b338bafe64ae
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-11 12:06:32 -0400

    Login page styling

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from "./hooks/useAuth"
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
     <NavigationContainer>
-    <AuthProvider>
-    <StackNavigator />
-    </AuthProvider>
+    <LoginScreen />
     </NavigationContainer>
   );
 }
