683fe5b523547b9c6de6042259eddc770d8d4355
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-16 09:59:27 -0400

    troubleshooting FireBase auth

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUp from './src/screens/SignUp';
-import { auth } from './hooks/useAuth';
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName = 'Login' >
+        <Stack.Screen name = "Home" component = {HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
