8286737da3e5b7a9d8fc71785905158e39577846
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-18 11:55:59 -0400

    authentication

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack'
 import LoginScreen from './src/screens/LoginScreen';
 import SignUp from './src/screens/SignUp';
 import HomeScreen from './src/screens/HomeScreen';
+import { AuthProvider } from './hooks/useAuth';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <AuthProvider>
       <Stack.Navigator initialRouteName = 'Login' >
-        <Stack.Screen name = "Home" component = {HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name = "Home" component = {HomeScreen} />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
+      </AuthProvider>
     </NavigationContainer>
   );
 }
