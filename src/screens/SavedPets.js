93e17a16141783c3a3b385df287917bd3f438c75
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-25 07:58:30 -0400

    SavedPets screen initial commit

---
+++ src/screens/SavedPets.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+
+//screen to display saved pets
+const SavedPets = () => {
+    return (
+      <View>
+        <Text>Saved Pets</Text>
+      </View>
+    );
+  };
+  export default SavedPets;
