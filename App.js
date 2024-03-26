f82f9007ff5ac12d39b7468d9932a05e6c445559
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-26 08:32:41 -0400

    login/logout functionality improved

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import { NavigationContainer } from '@react-navigation/native';;
 import HomeScreen from './src/screens/HomeScreen';
 import SavedPets from './src/screens/SavedPets';
 import { AuthProvider } from './hooks/useAuth';
+import useAuth from './hooks/useAuth';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchSettings from './src/screens/SearchSettings';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,11 +13,30 @@ export default function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <Tab.Navigator>
-        <Tab.Screen name = "Home" component = {HomeScreen} />
-        <Tab.Screen name = "My Pets" component = {SavedPets} />
-      </Tab.Navigator>
+        <AppContent />
       </AuthProvider>
     </NavigationContainer>
   );
 }
+
+const AppContent = () => {
+  const { user, loadingInitial } = useAuth();
+
+  if (loadingInitial) {
+    // You could return a loading spinner here
+    return null; // Or some loading component
+  }
+
+  return (
+    !user ? (
+      <LoginScreen />
+    ) : (
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="My Pets" component={SavedPets} />
+        <Tab.Screen name="Search Settings" component={SearchSettings} />
+      </Tab.Navigator>
+    )
+  );
+};
+    
