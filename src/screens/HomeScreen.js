1b84197e85cae6699c916f4b385f06d1435b58da
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-03-26 08:34:45 -0400

    Logout button added to homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView } from'react-native';
+import { ActivityIndicator, StyleSheet, View, ScrollView, TouchableOpacity, Text } from'react-native';
 import React, { useEffect, useState } from 'react';
 import PetCard from './components/PetCard'
-import Swiper from 'react-native-deck-swiper';
+import useAuth from '../../hooks/useAuth';
 
 const HomeScreen = () => {
 const [pets, setPets] = useState([]);//Create an array to hold the pets
+const { logout } = useAuth();
 
 //Fetch the pets from the server
 useEffect(() => {
@@ -21,14 +22,33 @@ useEffect(() => {
   }, []);
 
   return (
+    
     <ScrollView>
+      <TouchableOpacity onPress={logout} style={{ margineRight: 10}}>
+      <Text>LogOut</Text>
+    </TouchableOpacity>
       {pets.length > 0 ? (
         pets.map((pet) => <PetCard key={pet.id} pet={pet} />)
       ) : (
-        <Text>Loading pets...</Text>
+        <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size="large" color="#00ff00" />
+        </View>
       )}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
 
 export default HomeScreen;
