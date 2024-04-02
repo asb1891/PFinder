fab1a810f874e99d2faad1e034c5615e8db91a79
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-04-02 10:51:30 -0400

    firebase logout logic fixed

--- database/firebase.js
+++ database/firebase.js
@@ -1,10 +1,10 @@
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import firebase from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,10 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-  });
-
-export default { auth };
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
