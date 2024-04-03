e6891556528c18f42fcde346bec2e46c8d4bbe1e
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-04-03 10:30:16 -0400

    navigation folder initial commit

---
+++ src/navigation/StackNavigator.js
@@ -0,0 +1,22 @@
+// import { createStackNavigator } from '@react-navigation/stack';
+// const HomeStack = createStackNavigator();
+// const SavedPetsStack = createStackNavigator();
+
+// function HomeStackNavigator() {
+//   return (
+//     <HomeStack.Navigator>
+//       <HomeStack.Screen name="Home" component={HomeScreen} />
+//       // ... other screens in the home stack
+//     </HomeStack.Navigator>
+//   );
+// }
+
+// function SavedPetsStackNavigator() {
+//   return (
+//     <SavedPetsStack.Navigator>
+//       <SavedPetsStack.Screen name="SavedPets" component={SavedPetsScreen} />
+//       // ... other screens in the saved pets stack
+//     </SavedPetsStack.Navigator>
+//   );
+// }
+
