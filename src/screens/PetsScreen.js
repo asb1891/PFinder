3fa58ff78d34e49f06bc580afe24c4b115d12dbd
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-05-03 09:33:55 -0400

    Updated Tab Nav styling

--- src/screens/PetsScreen.js
+++ src/screens/PetsScreen.js
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { View, ActivityIndicator } from 'react-native';
-import Swiper from 'react-native-deck-swiper';
-import { fetchPets } from '../api';
-import PetCard from './components/PetCard'
+// import React, { useState, useEffect } from 'react';
+// import { View, ActivityIndicator } from 'react-native';
+// import Swiper from 'react-native-deck-swiper';
+// import { fetchPets } from '../api';
+// import PetCard from './components/PetCard'
 
-const PetsScreen = () => {
-    const [pets, setPets] = useState([]);
-    const [loading, setLoading] = useState(true);
+// const PetsScreen = () => {
+//     const [pets, setPets] = useState([]);
+//     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const loadPets = async () => {
-            try {
-                const fetchedPets = await fetchPets();
-                setPets(fetchedPets);
-            } catch (error) {
-                console.log(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        loadPets();
-    }, []);
+//     useEffect(() => {
+//         const loadPets = async () => {
+//             try {
+//                 const fetchedPets = await fetchPets();
+//                 setPets(fetchedPets);
+//             } catch (error) {
+//                 console.log(error);
+//             } finally {
+//                 setLoading(false);
+//             }
+//         };
+//         loadPets();
+//     }, []);
 
-    if (loading) {
-        return <ActivityIndicator size="large" color="#0000ff" />;
-    }
+//     if (loading) {
+//         return <ActivityIndicator size="large" color="#0000ff" />;
+//     }
 
-    return (
-        <View style={{flex: 1}}>
-            <Swiper 
-                cards={pets}
-                renderCard={(pet) => <PetCard pet={pet} />}
-                // You had "pets" here instead of "pet" in your renderCard function
-            />
-        </View>
-    );
-};
+//     return (
+//         <View>
+//             <Swiper 
+//                 cards={pets}
+//                 renderCard={(pet) => <PetCard pet={pet} />}
+//             />
+//         </View>
+//     );
+// };
 
-export default PetsScreen;
+// export default PetsScreen;
