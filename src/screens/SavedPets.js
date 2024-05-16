3a01d326106257b743dfcff69e0ff33f8a66fc21
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-05-16 13:58:49 -0400

    Saved Pet Screen, needs styling

--- src/screens/SavedPets.js
+++ src/screens/SavedPets.js
@@ -1,12 +1,38 @@
-import {View, Text} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { usePets } from "../../PetsContext";
 
-
-//screen to display saved pets
 const SavedPets = () => {
-    return (
-      <View>
-        <Text>Saved Pets</Text>
-      </View>
-    );
-  };
-  export default SavedPets;
+  const { savedPets } = usePets();
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => console.log("Pet selected", item.name)}>
+      <Image
+        source={{ uri: item.photos[0]?.medium }}
+        className="w-48 h-48 rounded-lg shadow-lg"
+      />
+      <Text>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View className="bg-white">
+      <Text className="text-center font-semibold">Saved Pets</Text>
+      <FlatList
+        data={savedPets}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        numColumns={2}
+      />
+    </View>
+  );
+};
+
+export default SavedPets;
