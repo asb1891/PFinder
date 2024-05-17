b83cd557445dcf3f14c70a413082a9fbb9c716ee
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-05-17 12:46:40 -0400

    updated styling for saved pets

--- src/screens/SavedPets.js
+++ src/screens/SavedPets.js
@@ -8,118 +8,111 @@ import {
   ScrollView,
   Modal, 
   StyleSheet,
-  Button
+  Button,
+  Linking
 } from "react-native";
 import { usePets } from "../../PetsContext";
+
+const debounce = (func, delay) => {
+  let inDebounce; // This is the timer
+  return function () {
+    const context = this; // This is the context
+    const args = arguments; // This is the arguments array
+    clearImmediate(inDebounce); // Clear the timer
+    inDebounce = setTimeout(() => func.apply(context, args), delay); // Set the timer
+  };
+};
 
 const SavedPets = () => {
   const { savedPets } = usePets();
   const [selectedPet, setSelectedPet] = useState(null);
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+
+  const handlePhotoChange = debounce(() => {
+    const nextIndex = (currentPhotoIndex + 1) % selectedPet.photos.length; // loop back to 0 if at the end of the array
+    setCurrentPhotoIndex(nextIndex); // Update the current photo index
+  }, 100); // Call the function after 400ms
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => setSelectedPet(item)}>
+    <TouchableOpacity onPress={() => { setSelectedPet(item); setCurrentPhotoIndex(0)}}>
       <Image
         source={{ uri: item.photos[0]?.medium }}
         className="m-1 w-48 h-64 rounded-lg shadow-lg"
       />
-      <Text style={{ textAlign: 'center', marginTop: 1, fontSize: '14' }}>{item.name}</Text>
+      <Text style={{ textAlign: 'center', marginTop: 1 }}>{item.name}</Text>
     </TouchableOpacity>
   );
 
+  const sendEmail = () => {
+    const email = selectedPet.contact.email;
+    const subject = `Inquiry about ${selectedPet.name}`;
+    const body = `Hello,\n\nI am interested in learning more about ${selectedPet.name}. Could you please provide more details?\n\nThank you!`;
+
+    const mailto = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    
+    Linking.canOpenURL(mailto)
+      .then((supported) => {
+        if (!supported) {
+          Alert.alert('Error', 'Email client is not available');
+        } else {
+          return Linking.openURL(mailto);
+        }
+      })
+      .catch((err) => console.error('Error opening email client', err));
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>Saved Pets</Text>
+<ScrollView className="bg-white">
+      <Text className="text-center font-semibold text-lg my-2">Saved Pets</Text>
       <FlatList
         data={savedPets}
         keyExtractor={(item) => item.id.toString()}
         renderItem={renderItem}
         numColumns={2}
-        contentContainerStyle={styles.flatListContent}
+        contentContainerStyle={{ paddingHorizontal: 16 }}
       />
       {selectedPet && (
         <Modal
-          visible={!!selectedPet} // If there is a selected pet, show the modal
-          transparent={true} // Make the modal transparent
-          animationType="slide" // Slide the modal in from the bottom
-          onRequestClose={() => setSelectedPet(null)} // When the modal is closed, set the selected pet to null
+          visible={!!selectedPet}
+          transparent={true}
+          animationType="slide"
+          onRequestClose={() => setSelectedPet(null)}
         >
-          <View style={styles.modalContainer}> 
-            <View style={styles.modalContent}>
-              <Image
-                source={{ uri: selectedPet.photos[0]?.large }}
-                className="w-64 h-80 rounded-lg shadow-lg"
-              />
-              <Text style={styles.modalText}>{selectedPet.name}</Text>
-              <Text style={styles.modalText}>{selectedPet.gender}</Text>
-              <Text style={styles.modalText}>{selectedPet.age}</Text>
-              <Text style={styles.modalText}>{selectedPet.contact.email}</Text>
-              <Text style={styles.modalText}>{selectedPet.contact.address.city}, {selectedPet.contact.address.state} {selectedPet.contact.address.postcode}</Text>
-              {/* <Text style={styles.modalText}>{selectedPet.description}</Text> */}
-              <Button title="Close" onPress={() => setSelectedPet(null)} />
+          <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+            <View className="w-72 p-5 bg-white rounded-lg items-center border border-gray-200 shadow-lg">
+              <TouchableOpacity onPress={handlePhotoChange} className="h-72 mb-4">
+                <Image
+                  source={{ uri: selectedPet.photos[currentPhotoIndex]?.large }}
+                  className="w-64 h-64 rounded-lg"
+                />
+              </TouchableOpacity>
+              <View className="flex-row justify-center mt-2 mb-4">
+                {selectedPet.photos.map((_, idx) => (
+                  <View
+                    key={idx}
+                    className={`h-2 w-2 mx-1 rounded-full ${idx === currentPhotoIndex ? 'bg-red-400' : 'bg-black'}`}
+                  />
+                ))}
+              </View>
+              <Text className="text-center text-lg mt-2">{selectedPet.name}</Text>
+              <Text className="text-center text-sm">{selectedPet.age}</Text>
+              <Text className="text-center text-sm">{selectedPet.gender}</Text>
+              <Text className="text-center text-sm">{selectedPet.contact.email}</Text>
+              <Text className="text-center text-sm">
+                Location: {selectedPet.contact.address.city}, {selectedPet.contact.address.state} {selectedPet.contact.address.postcode}
+              </Text>
+              <TouchableOpacity onPress={sendEmail} className="mt-5 bg-yellow-400 py-2 px-5 rounded">
+                <Text className="text-white text-lg">Send Email</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setSelectedPet(null)} className="mt-5 bg-red-500 py-2 px-5 rounded">
+                <Text className="text-white text-lg">Close</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </Modal>
       )}
     </ScrollView>
   );
-};
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-  },
-  title: {
-    textAlign: "center",
-    fontWeight: "600",
-    fontSize: 18,
-    marginVertical: 10,
-  },
-  flatListContent: {
-    paddingHorizontal: 16,
-  },
-  itemContainer: {
-    alignItems: "center",
-    margin: 10,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  text: {
-    textAlign: "center",
-    marginTop: 5,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  modalContent: {
-    width: 300,
-    padding: 20,
-    backgroundColor: "white",
-    borderRadius: 8,
-    alignItems: "center",
-  },
-  modalImage: {
-    width: 250,
-    height: 250,
-    borderRadius: 8,
-  },
-  modalText: {
-    textAlign: "center",
-    marginTop: 10,
-    fontSize: 16,
-  },
-});
+}
 
 export default SavedPets;
