a1ec7a3cd67d13d7f555c8c935f49251e781d21b
asb1891 <137960726+asb1891@users.noreply.github.com>
2025-01-20 13:05:57 -0500

    updated API routes for post commands

--- Server/src/routes/petRoutes.js
+++ Server/src/routes/petRoutes.js
@@ -1,10 +1,18 @@
 // defining the routes for the pets API
 import express from 'express';
-import { getPets } from '../controllers/petController.js';
+import { getPets, savePet, getAllSavedPets } from '../controllers/petController.js';
+
+// const { signupUser } = require('../controllers/petController.js');
 
 const router = express.Router(); // Create a new express router
 // Route for fetching pet data
 router.get('/pets', getPets);
+
+//router for saving a right swiped pet
+router.post('/pets', savePet);
+
+// Route for fetching saved pets
+router.get('/pets/saved', getAllSavedPets);
 
 export default router;
 
