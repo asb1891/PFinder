780502a62af13094cef00048bfb11df21bcfc2be
asb1891 <137960726+asb1891@users.noreply.github.com>
2025-01-22 14:34:17 -0500

    postgres implemented and tested

---
+++ Server/database/postgres/index.js
@@ -0,0 +1,19 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: process.env.DB_USER, // Your Postgres user
+    host: process.env.DB_HOST, // Your Postgres host
+    database: process.env.DB_DATABASE, // Your Postgres database
+    password: process.env.DB_PASSWORD, // Your Postgres password
+    port: process.env.DB_PORT, // Your Postgres port
+});
+
+export default {
+    query: (text, params) => pool.query(text, params),
+};
+
---
+++ Server/database/postgres/queries.js
@@ -0,0 +1,49 @@
+import db from './index.js';
+
+
+// Save a pet to the database (from a right swipe)
+export const saveSwipedPet = async (pet) => {
+    const query = `
+        INSERT INTO pets (
+            pet_id, type, name, species, age, gender, description, status, tags,
+            photo_urls, contact_email, contact_phone, contact_address1, contact_address2,
+            contact_city, contact_state, contact_postcode, contact_country
+        ) VALUES (
+            $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
+        )
+        ON CONFLICT (pet_id) DO NOTHING; -- Avoid duplicate entries
+    `;
+    const values = [
+        pet.id,
+        pet.type,
+        pet.name,
+        pet.species,
+        pet.age,
+        pet.gender,
+        pet.description,
+        pet.status || null,
+        pet.tags || null,
+        pet.photos.map((photo) => photo.full) || null,
+        pet.contact.email || null,
+        pet.contact.phone || null,
+        pet.contact.address.address1 || null,
+        pet.contact.address.address2 || null,
+        pet.contact.address.city || null,
+        pet.contact.address.state || null,
+        pet.contact.address.postcode || null,
+        pet.contact.address.country || null,
+    ];
+    await db.query(query, values);
+};
+
+
+// Retrieve all saved pets from the database
+export const getSavedPets = async () => {
+    const query = `
+        SELECT * FROM pets
+        ORDER BY created_at DESC;
+    `;
+    const result = await db.query(query);
+    return result.rows;
+};
+
