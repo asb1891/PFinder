d13f14ac205101883be8cd519ab8b29c732ad3b4
asb1891 <137960726+asb1891@users.noreply.github.com>
2025-02-11 09:43:13 -0500

    added code comments for better readability

--- Server/database/postgres/index.js
+++ Server/database/postgres/index.js
@@ -1,19 +1,19 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
+import pg from "pg";
+import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config(); // Load environment variables from a .env file
 
+// Create a new Pool instance
 const { Pool } = pg;
-
+// Create a new Pool instance
 const pool = new Pool({
-    user: process.env.DB_USER, // Your Postgres user
-    host: process.env.DB_HOST, // Your Postgres host
-    database: process.env.DB_DATABASE, // Your Postgres database
-    password: process.env.DB_PASSWORD, // Your Postgres password
-    port: process.env.DB_PORT, // Your Postgres port
+  user: process.env.DB_USER, // Your Postgres user
+  host: process.env.DB_HOST, // Your Postgres host
+  database: process.env.DB_DATABASE, // Your Postgres database
+  password: process.env.DB_PASSWORD, // Your Postgres password
+  port: process.env.DB_PORT, // Your Postgres port
 });
 
 export default {
-    query: (text, params) => pool.query(text, params),
+  query: (text, params) => pool.query(text, params),
 };
-
