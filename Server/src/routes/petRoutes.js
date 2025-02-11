714fefe0dacc908c51f2459a9ee31251fc6d939d
asb1891 <137960726+asb1891@users.noreply.github.com>
2025-02-11 09:45:28 -0500

    fixed route typo

--- Server/src/routes/petRoutes.js
+++ Server/src/routes/petRoutes.js
@@ -1,18 +1,21 @@
 // defining the routes for the pets API
-import express from 'express';
-import { getPets, savePet, getAllSavedPets } from '../controllers/petController.js';
+import express from "express";
+import {
+  getPets,
+  savePet,
+  getAllSavedPets,
+} from "../controllers/petController.js";
 
 // const { signupUser } = require('../controllers/petController.js');
 
 const router = express.Router(); // Create a new express router
 // Route for fetching pet data
-router.get('/pets', getPets);
+router.get("/pets", getPets);
 
 //router for saving a right swiped pet
-router.post('/pets', savePet);
+router.post("/pets", savePet);
 
 // Route for fetching saved pets
-router.get('/pets/saved', getAllSavedPets);
+router.get("/pets/saved", getAllSavedPets);
 
 export default router;
-
