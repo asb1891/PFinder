932b0f83e068a123ef2ec63d2a9b213baabce587
asb1891 <137960726+asb1891@users.noreply.github.com>
2025-02-11 19:32:02 -0500

    improved saved pets container styling

--- src/screens/SavedPets.js
+++ src/screens/SavedPets.js
@@ -10,8 +10,10 @@ import {
   StyleSheet,
   Button,
   Linking,
+  Alert,
 } from "react-native";
 import { usePets } from "../../PetsContext";
+import styles from "../assets/styles";
 
 const debounce = (func, delay) => {
   let inDebounce; // This is the timer
@@ -52,27 +54,10 @@ const SavedPets = () => {
           setSelectedPet(item);
           setCurrentPhotoIndex(0);
         }}
-        style={{
-          margin: 8,
-          width: "46%",
-          backgroundColor: "#facc15",
-          borderRadius: 10,
-          overflow: "hidden",
-          shadowColor: "#000",
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-          elevation: 5,
-        }}
+        style={styles.savedPetCard}
       >
-        <Image
-          source={{ uri: photoUri }}
-          style={{ width: "100%", height: 170 }}
-          resizeMode="cover"
-        />
-        <Text style={{ textAlign: "center", padding: 8, fontWeight: "bold" }}>
-          {item.name || "Unknown Name"} {/* Safeguard for missing name */}
-        </Text>
+        <Image source={{ uri: photoUri }} style={styles.savedPetImage} />
+        <Text style={styles.savedPetName}>{item.name || "Unknown Name"}</Text>
       </TouchableOpacity>
     );
   };
@@ -80,7 +65,7 @@ const SavedPets = () => {
   //function to send email via the saved pets card
   //linking to the default email client
   const sendEmail = () => {
-    const email = selectedPet.contact.email; //get the email from the selected pet api
+    const email = selectedPet.contact_email; //get the email from the selected pet api
     const subject = `Inquiry about ${selectedPet.name}`; //get the subject of the email
     //custom email body
     const body = `Hello,\n\nI am interested in learning more about ${selectedPet.name}. Could you please provide more details?\n\nThank you!`;
@@ -103,24 +88,16 @@ const SavedPets = () => {
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff" }}>
-      <Text
-        style={{
-          textAlign: "center",
-          fontWeight: "bold",
-          fontSize: 18,
-          marginVertical: 10,
-        }}
-      >
-        Saved Pets
-      </Text>
+    <View style={styles.screenBackground}>
+      <Text style={styles.savedPetsTitle}>Saved Pets</Text>
       <FlatList
-        data={savedPets || []} // Ensure savedPets is always an array
+        data={savedPets || []}
         keyExtractor={(item) => item.id?.toString() || Math.random().toString()}
         renderItem={renderItem}
         numColumns={2}
-        contentContainerStyle={{ paddingHorizontal: 16 }}
+        contentContainerStyle={styles.savedPetsContainer}
       />
+
       {selectedPet && (
         <Modal
           visible={!!selectedPet}
@@ -128,141 +105,43 @@ const SavedPets = () => {
           animationType="slide"
           onRequestClose={() => setSelectedPet(null)}
         >
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-            }}
-          >
-            <View
-              style={{
-                width: 300,
-                padding: 20,
-                backgroundColor: "white",
-                borderRadius: 10,
-                shadowColor: "#000",
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.25,
-                shadowRadius: 3.84,
-                elevation: 5,
-              }}
-            >
+          <View style={styles.modalBackground}>
+            <View style={styles.modalContainer}>
               <TouchableOpacity
                 onPress={handlePhotoChange}
-                style={{ marginBottom: 15 }}
+                style={styles.imageWrapper}
               >
                 <Image
-                  source={{
-                    uri:
-                      selectedPet?.photo_urls?.[currentPhotoIndex] ||
-                      "https://via.placeholder.com/350",
-                  }}
-                  style={{
-                    width: 250,
-                    height: 250,
-                    borderRadius: 10,
-                    marginBottom: 10,
-                    marginLeft: 5,
-                  }}
+                  source={{ uri: selectedPet?.photo_urls?.[currentPhotoIndex] }}
+                  style={styles.modalPetImage}
                 />
               </TouchableOpacity>
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  marginBottom: 15,
-                }}
-              >
-                {selectedPet.photo_urls.map((photo, index) => (
-                  <View
-                    key={index}
-                    style={{
-                      height: 10,
-                      width: 10,
-                      marginHorizontal: 5,
-                      borderRadius: 5,
-                      backgroundColor:
-                        index === currentPhotoIndex ? "red" : "black",
-                    }}
-                  />
-                ))}
-              </View>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontWeight: "bold",
-                  textAlign: "center",
-                  marginBottom: 5,
-                  fontFamily: "Arial",
-                }}
-              >
-                {selectedPet.name}
-              </Text>
-              <Text
-                style={{ fontSize: 14, textAlign: "center", marginBottom: 5 }}
-              >
-                {selectedPet.age}
-              </Text>
-              <Text
-                style={{ fontSize: 14, textAlign: "center", marginBottom: 5 }}
-              >
-                {selectedPet.gender}
-              </Text>
-              <Text
-                style={{ fontSize: 14, textAlign: "center", marginBottom: 5 }}
-              >
-                {selectedPet.contact?.email || "No email available"}
-              </Text>
-              <Text
-                style={{ fontSize: 14, textAlign: "center", marginBottom: 5 }}
-              >
-                {selectedPet.contact?.address?.city || "Unknown City"},
-                {selectedPet.contact?.address?.state || "Unknown State"}{" "}
-                {selectedPet.contact?.address?.postcode || ""}
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  textAlign: "center",
-                  marginBottom: 5,
-                  marginTop: 5,
-                }}
-              >
-                {truncateText(selectedPet.description, 1)}
-              </Text>
 
-              <TouchableOpacity
-                onPress={sendEmail}
-                style={{
-                  backgroundColor: "#facc15",
-                  paddingVertical: 10,
-                  paddingHorizontal: 20,
-                  borderRadius: 5,
-                  marginBottom: 10,
-                }}
-              >
-                <Text
-                  style={{ color: "white", fontSize: 16, textAlign: "center" }}
-                >
-                  Send Email
+              {/* Pet Info */}
+              <View style={styles.petInfoContainer}>
+                <Text style={styles.petName}>{selectedPet.name}</Text>
+                <Text style={styles.petDetails}>
+                  {selectedPet.age} • {selectedPet.gender}
                 </Text>
+                <Text style={styles.petLocation}>
+                  {selectedPet.contact?.address?.city || "Unknown City"},
+                  {selectedPet.contact?.address?.state || "Unknown State"}
+                  {selectedPet.contact?.address?.postcode || ""}
+                </Text>
+                <Text style={styles.petEmail}>
+                  {selectedPet.contact?.email || "No email available"}
+                </Text>
+              </View>
+
+              {/* Buttons */}
+              <TouchableOpacity onPress={sendEmail} style={styles.emailButton}>
+                <Text style={styles.emailButtonText}>Send Email</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => setSelectedPet(null)}
-                style={{
-                  backgroundColor: "#d9534f",
-                  paddingVertical: 10,
-                  paddingHorizontal: 20,
-                  borderRadius: 5,
-                }}
+                style={styles.closeButton}
               >
-                <Text
-                  style={{ color: "white", fontSize: 16, textAlign: "center" }}
-                >
-                  Close
-                </Text>
+                <Text style={styles.closeButtonText}>Close</Text>
               </TouchableOpacity>
             </View>
           </View>
